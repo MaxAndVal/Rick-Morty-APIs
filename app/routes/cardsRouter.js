8dba6f445c9ef9794e141c54eb0fff63b1c61069
zigoval <rouby.v@hotmail.com>
2019-01-21 15:21:26 +0100

    fix/ change json object to array

--- app/routes/cardsRouter.js
+++ app/routes/cardsRouter.js
@@ -13,35 +13,35 @@ const {
 
 cardsRoute.get("/search/:name", (req, res) => {
   getCardByName(req.params.name).then(response => {
-    res.json(response.data).catch(error => res.send(error));
+    res.json([response.data]).catch(error => res.send([error]));
   });
 });
 
 cardsRoute.get("/:id", (req, res) => {
   getCardsById(req.params.id)
     .then(response => {
-      res.json(response.data);
+      res.json([response.data]);
     })
-    .catch(error => res.json(error));
+    .catch(error => res.json([error]));
 });
 
 cardsRoute.get("/all/:page", (req, res) => {
   const page = req.params.page > 1 ? req.params.page : 1;
   getCardsByPages(page)
     .then(response => {
-      res.json(response.data);
+      res.json([response.data]);
     })
     .catch(error => {
-      res.json(error);
+      res.json([error]);
     });
 });
 
 cardsRoute.get("/randomDeckGenerator/:id", async (req, res) => {
   const user_id = req.params.id;
   checkDeckToOpen(user_id)
-    .then(response => res.json(response))
+    .then(response => res.json([response]))
     .catch(error => {
-      res.json(error);
+      res.json([error]);
     });
 });
 
