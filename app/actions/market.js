6b4dd16674713dc49a0ee36020a320dbf1b0f630
Valentin Rouby <valentin.rouby@plebicom.com>
2019-03-06 14:51:46 +0100

    feat(market) swap is okay

--- app/actions/market.js
+++ app/actions/market.js
@@ -1,6 +1,6 @@
 const connection = require("../../dbConnexion");
 const axios = require("axios");
-const {addCardToDeck} = require("./cards")
+const { addCardToDeck } = require("./cards");
 
 async function getMarketOfUser(user_id) {
   return new Promise((resolve, reject) => {
@@ -33,66 +33,106 @@ const addCardtoMarket = async (user_id, card_id, card_name, price) => {
     );
   });
 };
-const buyCard = async(user_id, friend_id, card_id, price) =>{
-  console.log("user_id", user_id)
-  console.log("friends", friend_id)
-  console.log("card",card_id)
-  console.log("prix", price)
-return new Promise (async (resolve, reject)=>{
-      connection.beginTransaction(function(err){
-        if(err){
-          reject(err)
-        }
-        let queryAmountU2= "SELECT amount FROM deck WHERE card_id=? AND user_id=?"
-        connection.query(queryAmountU2,[card_id, friend_id], (err, rows, fields)=>{
-          if(err){
+const buyCard = async (user_id, friend_id, card_id, price) => {
+  return new Promise(async (resolve, reject) => {
+    connection.beginTransaction(function(err) {
+      if (err) {
+        reject(err);
+      }
+      let queryAmountU2 =
+        "SELECT amount FROM deck WHERE card_id=? AND user_id=?";
+      connection.query(
+        queryAmountU2,
+        [card_id, friend_id],
+        (err, rows, fields) => {
+          if (err) {
             connection.rollback(function() {
               reject(err);
             });
-            reject(err)
+            reject(err);
           }
-          if(!rows){
-            reject("undifined")
-          }
-          console.log("rows : ", rows[0].amount)
-          const actualAmount = rows[0].amount;
-          if(actualAmount>0){
-          queryAmountU2 = "UPDATE deck SET amount = ? WHERE card_id =? and user_id=?"
-          connection.query(queryAmountU2, [actualAmount-1, card_id, friend_id], (err, rows, fields)=>{
-            if(err){
-              connection.rollback(function() {
-                reject(err);
-              });
+          if (rows === undefined) {
+            connection.rollback(function() {
               reject(err);
-            }
-            console.log(user_id, card_id)
-            addCardToDeck(user_id, card_id).then(res =>
-              {
-                const result = res
-                if(result!="ok"){
+            });
+            return reject("undifined");
+          }
+          const actualAmount = rows[0].amount;
+          if (actualAmount > 0) {
+            queryAmountU2 =
+              "UPDATE deck SET amount = ? WHERE card_id =? and user_id=?";
+            connection.query(
+              queryAmountU2,
+              [actualAmount - 1, card_id, friend_id],
+              (err, rows, fields) => {
+                if (err) {
                   connection.rollback(function() {
                     reject(err);
                   });
                   reject(err);
                 }
+                addCardToDeck(user_id, card_id).then(res => {
+                  const result = res;
+                  if (result != "ok") {
+                    connection.rollback(function() {
+                      reject(err);
+                    });
+                    reject(err);
+                  }
+                });
+                connection.query(
+                  "UPDATE users SET user_wallet=user_wallet+? where user_id=?",
+                  [price, friend_id],
+                  (err, rows, fields) => {
+                    if (err) {
+                      connection.rollback(function() {
+                        reject(err);
+                      });
+                      reject(err);
+                    }
+                  }
+                );
+                connection.query(
+                  "UPDATE users SET user_wallet=user_wallet-? where user_id=?",
+                  [price, user_id],
+                  (err, rows, fields) => {
+                    if (err) {
+                      connection.rollback(function() {
+                        reject(err);
+                      });
+                      reject(err);
+                    }
+                  }
+                );
+                connection.query(
+                  "DELETE from market where user_id=? AND card_id=? AND price=? LIMIT 1",
+                  [friend_id, card_id, price],
+                  (err, rows, fields) => {
+                    if (err) {
+                      connection.rollback(function() {
+                        reject(err);
+                      });
+                      reject(err);
+                    }
+                  }
+                );
                 connection.commit(function(err) {
-                  if (err) { 
+                  if (err) {
                     connection.rollback(function() {
                       throw err;
                     });
                   }
-                  console.log('Transaction Complete.');
-                  resolve(result+"tc")
+                  resolve({ code: 200, message: "F*** i hope it's okay" });
                 });
               }
-              )
-          })
-          }else{
-            reject({code:400, message:"erreur amount < 0"})
+            );
+          } else {
+            reject({ code: 400, message: "erreur amount < 0" });
           }
-        })
-      })
-  })
-}
+        }
+      );
+    });
+  });
+};
 
-module.exports = { getMarketOfUser, addCardtoMarket ,buyCard};
+module.exports = { getMarketOfUser, addCardtoMarket, buyCard };
