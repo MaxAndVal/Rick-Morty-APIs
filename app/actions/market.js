37c11dcb865a572f3b295e26c4f37fb2b96de0c3
zigoval <rouby.v@hotmail.com>
2019-03-11 10:25:18 +0100

    fixx(market) fix card_name undefined

--- app/actions/market.js
+++ app/actions/market.js
@@ -39,99 +39,94 @@ const buyCard = async (user_id, friend_id, card_id, price) => {
       if (err) {
         reject(err);
       }
-      let queryAmountU2 =
-        "SELECT amount, card_name FROM deck WHERE card_id=? AND user_id=?";
-      connection.query(
-        queryAmountU2,
-        [card_id, friend_id],
-        (err, rows, fields) => {
-          if (err) {
-            connection.rollback(function() {
-              reject(err);
-            });
+      let queryAmountU2 = "SELECT amount, card_name FROM deck WHERE card_id=? AND user_id=?";
+      connection.query(queryAmountU2, [card_id, friend_id], (err, rows, fields) => {
+        if (err) {
+          connection.rollback(function() {
             reject(err);
-          }
-          if (rows === undefined) {
-            connection.rollback(function() {
-              reject(err);
-            });
-            return reject("undifined");
-          }
-          const actualAmount = rows[0].amount;
-          if (actualAmount > 0) {
-            queryAmountU2 =
-              "UPDATE deck SET amount = ? WHERE card_id =? and user_id=?";
-            connection.query(
-              queryAmountU2,
-              [actualAmount - 1, card_id, friend_id],
-              (err, rows, fields) => {
-                if (err) {
+          });
+          reject(err);
+        }
+        if (rows === undefined) {
+          connection.rollback(function() {
+            reject(err);
+          });
+          return reject("undifined");
+        }
+        const actualAmount = rows[0].amount;
+        const card_name = rows[0].card_name;
+
+        if (actualAmount > 0) {
+          queryAmountU2 = "UPDATE deck SET amount = ? WHERE card_id =? and user_id=?";
+          connection.query(
+            queryAmountU2,
+            [actualAmount - 1, card_id, friend_id],
+            (err, rows, fields) => {
+              if (err) {
+                connection.rollback(function() {
+                  reject(err);
+                });
+                reject(err);
+              }
+              addCardToDeck(user_id, card_id, card_name).then(res => {
+                const result = res;
+                if (result != "ok") {
                   connection.rollback(function() {
                     reject(err);
                   });
                   reject(err);
                 }
-                const card_name = rows[0].card_name;
-                addCardToDeck(user_id, card_id, card_name).then(res => {
-                  const result = res;
-                  if (result != "ok") {
+              });
+              connection.query(
+                "UPDATE users SET user_wallet=user_wallet+? where user_id=?",
+                [price, friend_id],
+                (err, rows, fields) => {
+                  if (err) {
                     connection.rollback(function() {
                       reject(err);
                     });
                     reject(err);
                   }
-                });
-                connection.query(
-                  "UPDATE users SET user_wallet=user_wallet+? where user_id=?",
-                  [price, friend_id],
-                  (err, rows, fields) => {
-                    if (err) {
-                      connection.rollback(function() {
-                        reject(err);
-                      });
-                      reject(err);
-                    }
-                  }
-                );
-                connection.query(
-                  "UPDATE users SET user_wallet=user_wallet-? where user_id=?",
-                  [price, user_id],
-                  (err, rows, fields) => {
-                    if (err) {
-                      connection.rollback(function() {
-                        reject(err);
-                      });
-                      reject(err);
-                    }
-                  }
-                );
-                connection.query(
-                  "DELETE from market where user_id=? AND card_id=? AND price=? LIMIT 1",
-                  [friend_id, card_id, price],
-                  (err, rows, fields) => {
-                    if (err) {
-                      connection.rollback(function() {
-                        reject(err);
-                      });
-                      reject(err);
-                    }
-                  }
-                );
-                connection.commit(function(err) {
+                }
+              );
+              connection.query(
+                "UPDATE users SET user_wallet=user_wallet-? where user_id=?",
+                [price, user_id],
+                (err, rows, fields) => {
                   if (err) {
                     connection.rollback(function() {
-                      throw err;
+                      reject(err);
                     });
+                    reject(err);
                   }
-                  resolve({ code: 200, message: "F*** i hope it's okay" });
-                });
-              }
-            );
-          } else {
-            reject({ code: 400, message: "erreur amount < 0" });
-          }
+                }
+              );
+              connection.query(
+                "DELETE from market where user_id=? AND card_id=? AND price=? LIMIT 1",
+                [friend_id, card_id, price],
+                (err, rows, fields) => {
+                  if (err) {
+                    connection.rollback(function() {
+                      reject(err);
+                    });
+                    reject(err);
+                  }
+                }
+              );
+              connection.commit(function(err) {
+                if (err) {
+                  connection.rollback(function() {
+                    throw err;
+                  });
+                }
+                resolve({ code: 200, message: "F*** i hope it's okay" });
+              });
+            }
+          );
+        } else {
+          reject({ code: 400, message: "erreur amount < 0" });
         }
-      );
+      });
     });
   });
 };
