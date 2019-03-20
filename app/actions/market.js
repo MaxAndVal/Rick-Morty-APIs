e3ec535fdb3021d300ff0525a0174cad999c7e90
zigoval <rouby.v@hotmail.com>
2019-03-20 10:47:45 +0100

    add real import

--- app/actions/market.js
+++ app/actions/market.js
@@ -1,6 +1,6 @@
 const connection = require("../../dbConnexion");
 const axios = require("axios");
-const { addCardToDeck, getCardsById } = require("./cards");
+const { addCardToDeck, getDeckById } = require("./cards");
 
 async function getMarketOfUser(user_id) {
   return new Promise((resolve, reject) => {
