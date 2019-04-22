b8659f553c531e40adf574b6dcc0cdad05c6bdd7
Zigoval <rouby.v@hotmail.com>
2019-04-22 11:48:25 +0200

    ft(postman) add collection postman

--- app/constants/CodeHTTP.js
+++
@@ -1,9 +0,0 @@
-const codeHTTP = {
-  200: { code: 200, message: "Succeded" },
-  201: { code: 201, message: "Created" },
-  202: { code: 202, message: "Accepted" },
-  //203:{code:203, message:"Accepted"},
-  204: { code: 204, message: "No Content" },
-  250: { code: 250, message: "User not found" },
-  251: { code: 251, message: "Accepted" }
-};
