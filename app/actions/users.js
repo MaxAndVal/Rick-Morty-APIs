9c7f774f311f9b539ba01526f285453045a6cf35
MALOSSE MAXIME p1713335 <maxime.malosse@etu.univ-lyon1.fr>
2019-04-23 23:18:49 +0200

    fix/ remove HTTP code import

--- app/actions/users.js
+++ app/actions/users.js
@@ -1,6 +1,5 @@
 const connection = require("../../dbConnexion");
 const omit = require("object.omit");
-const CodeHTTP = require("../constants/CodeHTTP");
 const bcrypt = require("bcrypt");
 const saltRounds = 6;
 const { welcomeMail } = require("../utils/mailUtils.js");
@@ -36,23 +35,32 @@ async function createUser(user_name, user_email, user_password) {
                     code: 200,
                     message: "user is created",
                     user: rows[0]
-                  }).catch(err => reject({ code: 501, msg: "create user failed", err }))
+                  }).catch(err =>
+                    reject({ code: 501, msg: "create user failed", err })
+                  )
                 )
-                .catch(err => reject({ code: 502, msg: "create user failed", err }))
+                .catch(err =>
+                  reject({ code: 502, msg: "create user failed", err })
+                )
             )
-            .catch(err => reject({ code: 503, msg: "create user failed", err }));
+            .catch(err =>
+              reject({ code: 503, msg: "create user failed", err })
+            );
         } else {
           reject({ code: 204, message: "Email is already used" });
         }
       })
-      .catch(err => reject({ code: 501, msg: "create user failed before insert", err }));
+      .catch(err =>
+        reject({ code: 501, msg: "create user failed before insert", err })
+      );
   });
 }
 
 async function selectUserByEmail(user_email) {
   return new Promise((resolve, reject) => {
     console.log("user email : ", user_email);
-    const queryString = "SELECT * FROM users WHERE user_email=? AND external_id IS NULL";
+    const queryString =
+      "SELECT * FROM users WHERE user_email=? AND external_id IS NULL";
     connection.query(queryString, [user_email], (err, rows, fiels) => {
       if (err) {
         console.log("error in selectUserByEmail :", err);
@@ -82,7 +90,13 @@ async function getDeckToOpen(user_id) {
   });
 }
 
-function insertNewUser(user_name, user_email, user_password, external_id, user_image) {
+function insertNewUser(
+  user_name,
+  user_email,
+  user_password,
+  external_id,
+  user_image
+) {
   return new Promise((resolve, reject) => {
     const queryString =
       "INSERT INTO users (user_name, user_email, user_password, deckToOpen, external_id, user_image) VALUES (?,?,?,1,?,?)";
@@ -165,7 +179,9 @@ function deleteUserById(user_id) {
   return new Promise((resolve, reject) =>
     connection.query(queryString, [user_id], (err, rows, fields) => {
       if (err) {
-        console.log("failed to delete user with id " + user_id + "error : " + err);
+        console.log(
+          "failed to delete user with id " + user_id + "error : " + err
+        );
         reject({ code: 500, message: err.errorno });
       } else {
         if (rows.affectedRows == 0) {
